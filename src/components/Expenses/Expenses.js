25754a6e4726677d8e3e8187444b83d7977d5b47
joao.duarte.ey <joao.duarte@br.ey.com>
2023-02-07 17:29:54 -0300

    simple commit

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,25 +6,27 @@ import { useState } from 'react';
 
 const Expenses = (props) => {
 
-    const [filteredYear, setFilteredYear] = useState('2021')
+    const [filteredYear, setFilteredYear] = useState('2023')
 
     const addFilter = (selectedYear) =>{
         setFilteredYear(selectedYear);
     }
 
+    const filteredExpenses = props.expenses.filter(item => item.date.getFullYear() === parseInt(filteredYear));
+
+    let expensesContent = <p>No expenses found!</p>;
+
+    if(filteredExpenses.length > 0){
+        expensesContent = filteredExpenses.map((expense) => (
+        <ExpenseItem key={expense.id} title={expense.title} 
+        amount={expense.amount} date={expense.date} />
+        ))}
+
     return(
-        <div>
+        <Card className='expenses'>
             <ExpensesFilter selected={filteredYear} onSelectYear={addFilter} />
-            <Card className='expenses'>
-                {props.expenses
-                .filter(item => item.date.getFullYear() === parseInt(filteredYear))
-                .map((expense) => (
-                <ExpenseItem key={expense.id} title={expense.title} 
-                amount={expense.amount} date={expense.date} />
-                ))
-                }
-            </Card>
-        </div>
+            {expensesContent}
+        </Card>
     )
 }
 
